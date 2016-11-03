2c2b0f24cad712716b7b6404ed8ae7f34f7d7e65
kf-ireneuszpatalas <ipatalas@objectivity.co.uk>
2016-11-03 16:26:38 +0100

    Implemented component binding intellisense

--- component-intellisense/src/completionProvider.ts
+++ component-intellisense/src/completionProvider.ts
@@ -1,6 +1,10 @@
 import * as vsc from 'vscode';
-import { IComponentBinding } from './component';
+import * as _ from 'lodash';
+import { IComponentBinding, Component } from './component';
 import { ComponentScanner } from './componentScanner';
+
+const REGEX_TAG_NAME = /<([a-z-]+)/i;
+const REGEX_ATTRIBUTE_NAME = /([a-z-]+)=/gi;
 
 export class CompletionProvider implements vsc.CompletionItemProvider {
 	private scanner: ComponentScanner;
@@ -15,48 +19,100 @@ export class CompletionProvider implements vsc.CompletionItemProvider {
 	}
 
 	provideCompletionItems = (document: vsc.TextDocument, position: vsc.Position, token: vsc.CancellationToken): vsc.CompletionItem[] | Thenable<vsc.CompletionItem[]> | vsc.CompletionList | Thenable<vsc.CompletionList> => {
-		let hasOpeningTag = false;
-
 		let line = document.lineAt(position.line);
-		let openingTag = line.text.lastIndexOf("<", position.character);
-		let closingTag = line.text.indexOf(">", position.character);
 
-		if (openingTag > -1) {
+		let openingTagBefore = line.text.lastIndexOf("<", position.character);
+		let closingTagBefore = line.text.lastIndexOf(">", position.character);
+
+		let openingTagAfter = line.text.indexOf("<", position.character);
+		let closingTagAfter = line.text.indexOf(">", position.character);
+
+		// TODO:
+		// - handle closing tag - should not show any completions
+		// - handle cursor between two html tags
+
+		if (this.isInsideATag(openingTagBefore, closingTagBefore, openingTagAfter, closingTagAfter)) {
 			// get everything from starting < tag till the cursor
-			let openingTagTextRange = new vsc.Range(position.with(undefined, openingTag), position);
-			let text = document.getText(openingTagTextRange);
+			let tagTextRange = new vsc.Range(position.with(undefined, openingTagBefore), position.with(undefined, closingTagAfter + 1));
+			let text = document.getText(tagTextRange);
 
-			if (/^<[a-z]*$/i.test(text)) {
-				hasOpeningTag = true;
+			let { tag, attributes } = this.parseTag(text);
+
+			let component = this.scanner.components.find(c => c.htmlName === tag);
+			if (component) {
+				return this.provideAttributeCompletions(component, attributes);
 			}
+
+			return [];
 		}
 
-		// let range = document.getWordRangeAtPosition(position);
-		// let text = document.getText(range);
+		return this.provideTagCompletions(document, position, openingTagBefore);
+	}
 
-		return this.scanner.components.map(
-			(c, i): any => {
-				var bindings: string = "";
-				c.bindings.forEach(p => {
-					bindings += `${p.htmlName}="" `;
-				});
+	private parseTag = (text: string) => {
+		let match: RegExpExecArray;
+		match = REGEX_TAG_NAME.exec(text);
+		let tag = match[1];
 
-				let item = new vsc.CompletionItem(c.htmlName, vsc.CompletionItemKind.Class);
-				item.insertText = `<${c.htmlName} ${bindings.trim()}></${c.htmlName}>`;
+		let existingAttributes = [];
+		while (match = REGEX_ATTRIBUTE_NAME.exec(text)) {
+			existingAttributes.push(match[1]);
+		}
 
-				if (c.bindings.length > 0) {
-					item.documentation = c.bindings.reduce((acc: string, current: IComponentBinding) => {
-						return acc + `  ${current.htmlName}: ${current.type}\n`;
-					}, "Component bindings:\n");
-				}
+		return { tag, attributes: existingAttributes };
+	}
 
-				if (hasOpeningTag) {
-					item.additionalTextEdits = [
-						vsc.TextEdit.delete(new vsc.Range(position.translate(0, -1), position))
-					];
-				}
+	private isInsideATag = (openingTagBefore: number, closingTagBefore: number, openingTagAfter: number, closingTagAfter: number) => {
+		return openingTagBefore > -1 && closingTagAfter > -1;
+	}
+
+	provideAttributeCompletions = (component: Component, existingAttributes: string[]): vsc.CompletionItem[] => {
+		let attributes = _(existingAttributes);
+
+		return component.bindings
+			.filter(b => !attributes.includes(b.htmlName))
+			.map(b => {
+				let item = new vsc.CompletionItem(b.htmlName, vsc.CompletionItemKind.Field);
+				item.insertText = `${b.htmlName}=""`;
+				item.detail = "Component binding";
+				item.documentation = `Binding type: ${b.type}`;
+				item.label = ` ${b.htmlName}`; // space at the beginning so that these bindings are first on the list
 
 				return item;
 			});
+	}
+
+	provideTagCompletions = (document: vsc.TextDocument, position: vsc.Position, openingTagBeforeCursor: number): vsc.CompletionItem[] => {
+		let removeOpeningTag = false;
+
+		if (openingTagBeforeCursor > -1) {
+			// get everything from starting < tag till the cursor
+			let openingTagTextRange = new vsc.Range(position.with(undefined, openingTagBeforeCursor), position);
+			let text = document.getText(openingTagTextRange);
+
+			if (/^<[a-z]*$/i.test(text)) {
+				removeOpeningTag = true;
+			}
+		}
+
+		return this.scanner.components.map(c => {
+			var bindings = c.bindings.map(b => `${b.htmlName}=""`).join(' ');
+
+			let item = new vsc.CompletionItem(c.htmlName, vsc.CompletionItemKind.Class);
+			item.insertText = `<${c.htmlName} ${bindings.trim()}></${c.htmlName}>`;
+
+			if (c.bindings.length > 0) {
+				item.documentation = "Component bindings:\n"
+					+ c.bindings.map(b => `  ${b.htmlName}: ${b.type}`).join("\n");
+			}
+
+			if (removeOpeningTag) {
+				item.additionalTextEdits = [
+					vsc.TextEdit.delete(new vsc.Range(position.translate(0, -1), position))
+				];
+			}
+
+			return item;
+		});
 	}
 }
