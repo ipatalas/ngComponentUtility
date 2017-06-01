427fab5e2cf9d9eedd3d4ef7627ff419c1a8bdf7
ipatalas <fido@list.pl>
2017-06-01 23:58:41 +0200

    Fixed issue #13 - multiple chained component registrations

--- src/utils/component/componentParser.ts
+++ src/utils/component/componentParser.ts
@@ -6,16 +6,18 @@ import { SourceFile } from "../sourceFile";
 import { Controller } from "../controller/controller";
 import { Component, IComponentTemplate, IComponentBinding } from "./component";
 import { workspaceRoot } from '../vsc';
-import { TypescriptParser } from "../typescriptParser";
+import { TypescriptParser } from '../typescriptParser';
 import { ConfigParser } from '../configParser';
 
 export class ComponentParser {
 	private results: Component[] = [];
 	private tsParser: TypescriptParser;
+	private componentTsParser: Map<string, TypescriptParser>;
 	private isImported: boolean = false;
 
 	constructor(private file: SourceFile, private controllers: Controller[]) {
 		this.tsParser = new TypescriptParser(file.sourceFile);
+		this.componentTsParser = new Map<string, TypescriptParser>();
 	}
 
 	public parse = async () => {
@@ -32,12 +34,14 @@ export class ComponentParser {
 				&& (call.expression as ts.PropertyAccessExpression).name.text === 'component'
 				&& call.arguments.length === 2) {
 				const componentNameNode = call.arguments[0];
-				const componentConfigObj = await this.getComponentConfig(call.arguments[1]);
+				const componentConfigObj = await this.getComponentConfig(call.arguments[1], componentNameNode);
 
 				const component = this.createComponent(componentNameNode, componentConfigObj);
 				if (component) {
 					this.results.push(component);
 				}
+
+				await Promise.all(call.expression.getChildren().map(item => this.parseChildren(item)));
 			} else {
 				await Promise.all(call.getChildren().map(item => this.parseChildren(item)));
 			}
@@ -48,8 +52,10 @@ export class ComponentParser {
 		}
 	}
 
-	private getComponentConfig = async (configNode: ts.Expression): Promise<ts.ObjectLiteralExpression | ts.ClassDeclaration> => {
+	private getComponentConfig = async (configNode: ts.Expression, componentNameNode: ts.Expression): Promise<ts.ObjectLiteralExpression | ts.ClassDeclaration> => {
+		const componentName = this.tsParser.getStringValueFromNode(componentNameNode);
 		const componentConfigObj = this.tsParser.getObjectLiteralValueFromNode(configNode);
+
 		if (componentConfigObj) {
 			return Promise.resolve(componentConfigObj);
 		}
@@ -58,8 +64,8 @@ export class ComponentParser {
 			const newExpression = configNode as ts.NewExpression;
 			const parser = await this.getImportedFileParser(newExpression.expression as ts.Identifier);
 			if (parser) {
-				this.tsParser = parser;
-				const classDeclaration = this.tsParser.getExportedClass(this.tsParser.sourceFile, (newExpression.expression as ts.Identifier).text);
+				this.componentTsParser.set(componentName, parser);
+				const classDeclaration = parser.getExportedClass(parser.sourceFile, (newExpression.expression as ts.Identifier).text);
 				if (classDeclaration) {
 					this.isImported = true;
 					return Promise.resolve(classDeclaration);
@@ -70,8 +76,8 @@ export class ComponentParser {
 		if (configNode.kind === ts.SyntaxKind.Identifier) {
 			const parser = await this.getImportedFileParser(configNode as ts.Identifier);
 			if (parser) {
-				this.tsParser = parser;
-				const varDeclaration = this.tsParser.getExportedVariable(this.tsParser.sourceFile, (configNode as ts.Identifier).text);
+				this.componentTsParser.set(componentName, parser);
+				const varDeclaration = parser.getExportedVariable(parser.sourceFile, (configNode as ts.Identifier).text);
 				if (varDeclaration && varDeclaration.initializer.kind === ts.SyntaxKind.ObjectLiteralExpression) {
 					this.isImported = true;
 					return Promise.resolve(varDeclaration.initializer as ts.ObjectLiteralExpression);
@@ -106,11 +112,13 @@ export class ComponentParser {
 			return undefined;
 		}
 
+		const parser = this.componentTsParser.get(componentName) || this.tsParser;
+
 		const component = new Component();
-		component.path = this.tsParser.sourceFile.fullpath;
+		component.path = parser.sourceFile.fullpath;
 		component.name = componentName;
 		component.htmlName = decamelize(componentName, '-');
-		component.pos = this.tsParser.sourceFile.getLineAndCharacterOfPosition(
+		component.pos = parser.sourceFile.getLineAndCharacterOfPosition(
 			this.isImported ? (configObj.name || configObj).pos : componentNameNode.pos
 		);
 
@@ -118,12 +126,12 @@ export class ComponentParser {
 		const bindingsObj = config.get('bindings');
 		if (bindingsObj) {
 			const bindingsProps = bindingsObj as ts.ObjectLiteralExpression;
-			component.bindings.push(...bindingsProps.properties.map(this.createBinding));
+			component.bindings.push(...bindingsProps.properties.map(b => this.createBinding(b as ts.PropertyAssignment, parser)));
 		}
 
-		component.template = this.createTemplateFromUrl(config.get('templateUrl'));
+		component.template = this.createTemplateFromUrl(config.get('templateUrl'), parser);
 		if (!component.template) {
-			component.template = this.createTemplate(config.get('template'));
+			component.template = this.createTemplate(config.get('template'), parser);
 		}
 
 		component.controllerAs = this.createControllerAlias(config.get('controllerAs'));
@@ -151,13 +159,13 @@ export class ComponentParser {
 		}
 	}
 
-	private createTemplate = (node: ts.Expression): IComponentTemplate => {
+	private createTemplate = (node: ts.Expression, parser: TypescriptParser): IComponentTemplate => {
 		if (!node) {
 			return undefined;
 		}
 
 		if (node.kind === ts.SyntaxKind.StringLiteral || node.kind === ts.SyntaxKind.NoSubstitutionTemplateLiteral) {
-			const pos = this.tsParser.sourceFile.getLineAndCharacterOfPosition(node.getStart(this.tsParser.sourceFile));
+			const pos = parser.sourceFile.getLineAndCharacterOfPosition(node.getStart(parser.sourceFile));
 			const literal = node as ts.LiteralExpression;
 
 			return { path: this.file.path, pos, body: literal.text } as IComponentTemplate;
@@ -166,19 +174,19 @@ export class ComponentParser {
 			const call = node as ts.CallExpression;
 			if (call.arguments.length === 1 && call.expression.kind === ts.SyntaxKind.Identifier && call.expression.getText() === "require") {
 				const relativePath = (call.arguments[0] as ts.StringLiteral).text;
-				const templatePath = path.join(path.dirname(this.tsParser.path), relativePath);
+				const templatePath = path.join(path.dirname(parser.path), relativePath);
 
 				return { path: templatePath, pos: { line: 0, character: 0 } } as IComponentTemplate;
 			}
 		}
 	}
 
-	private createTemplateFromUrl(node: ts.Expression) {
+	private createTemplateFromUrl(node: ts.Expression, parser: TypescriptParser) {
 		if (!node) {
 			return undefined;
 		}
 
-		const value = this.tsParser.getStringValueFromNode(node);
+		const value = parser.getStringValueFromNode(node);
 		if (value) {
 			const templatePath = path.join(workspaceRoot, value);
 
@@ -186,14 +194,14 @@ export class ComponentParser {
 		}
 	}
 
-	private createBinding = (node: ts.PropertyAssignment): IComponentBinding => {
+	private createBinding = (node: ts.PropertyAssignment, parser: TypescriptParser): IComponentBinding => {
 		const { type, name } = this.parseType((node.initializer as ts.StringLiteral).text);
 
 		const binding = {} as IComponentBinding;
-		binding.name = node.name.getText(this.tsParser.sourceFile);
+		binding.name = node.name.getText(parser.sourceFile);
 		binding.type = type;
 		binding.htmlName = decamelize(name || binding.name, '-');
-		binding.pos = this.tsParser.sourceFile.getLineAndCharacterOfPosition(node.initializer.pos);
+		binding.pos = parser.sourceFile.getLineAndCharacterOfPosition(node.initializer.pos);
 
 		return binding;
 	}
