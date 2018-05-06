a947e7054cfbea936f8ddb0520c2dca629a2789a
ipatalas <fido@list.pl>
2018-05-06 22:33:42 +0200

    Added new tests for routes

--- test/utils/route.test.ts
+++ test/utils/route.test.ts
@@ -1,6 +1,8 @@
 import { getRouteSourceFile } from './helpers';
 import { Route } from '../../src/utils/route/route';
 import * as assert from 'assert';
+import * as path from 'path';
+import { TEST_ROOT } from '../../src/utils/vsc';
 
 describe('Given Route class', () => {
 	describe('when calling parse()', () => {
@@ -13,6 +15,26 @@ describe('Given Route class', () => {
 			assert.equal(route.name, 'example_route');
 			assert.equal(route.template.body, 'Route inline template');
 			assert.equal(route.template.path, sourceFile.path);
+		});
+
+		it('on route with external template then template is set correctly', async () => {
+			const sourceFile = getRouteSourceFile('route.external_template.ts');
+
+			const [route] = await Route.parse(sourceFile);
+
+			assert.equal(route.path, sourceFile.path);
+			assert.equal(route.name, 'example_route');
+			assert.equal(route.template.path, path.join(TEST_ROOT, 'subdir/template.html'));
+		});
+
+		it('on route with required template then template is set correctly', async () => {
+			const sourceFile = getRouteSourceFile('route.required_template.ts');
+
+			const [route] = await Route.parse(sourceFile);
+
+			assert.equal(route.path, sourceFile.path);
+			assert.equal(route.name, 'example_route');
+			assert.equal(route.template.path, path.join(path.dirname(sourceFile.path), 'subdir/template.html'));
 		});
 	});
 });
