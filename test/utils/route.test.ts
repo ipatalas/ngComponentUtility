8be214d8ba981791e8d417c2c698d6bd45753f0b
ipatalas <fido@list.pl>
2018-05-06 20:13:29 +0200

    Add missing test for Route parsing

---
+++ test/utils/route.test.ts
@@ -0,0 +1,17 @@
+import { getRouteSourceFile } from './helpers';
+import { Route } from '../../src/utils/route/route';
+import * as assert from 'assert';
+
+describe('Given Route class', () => {
+	describe('when calling parse()', () => {
+		it('on route with inline template then template is set correctly', async () => {
+			const sourceFile = getRouteSourceFile('route.inline_template.ts');
+
+			const [route] = await Route.parse(sourceFile);
+
+			assert.equal(route.name, 'example_route');
+			assert.equal(route.template.body, 'Route inline template');
+			assert.equal(route.template.path, sourceFile.path);
+		});
+	});
+});
